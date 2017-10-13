f21475d8dd141ff52cb6e572ddd1f582830c2fa1
Michał Gawron <arkezar@gmail.com>
2017-10-13 17:28:41 +0200

    Add program configuration file

--- FirebirdSql.Metadata.Comparer/Program.cs
+++ FirebirdSql.Metadata.Comparer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace FirebirdSql.Metadata.Comparer
 {
@@ -6,7 +8,14 @@ namespace FirebirdSql.Metadata.Comparer
     {
         static void Main(string[] args)
         {
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Console.WriteLine(config["Database:ConnectionString"]);
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
